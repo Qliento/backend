1ce5395e78c3a8bab6d56d862244f10933f12a4d
Nazgul <ismailova_n@auca.kg>
2020-10-28 21:07:57 +0600

    change url for images

--- post/urls.py
+++ post/urls.py
@@ -5,10 +5,9 @@ from .views import *
 app_name = 'post'
 urlpatterns = [
     path('about-us/', InfoView.as_view()),
-    path('analytics/', PostListView.as_view()),
+    path('blog/', PostListView.as_view()),
     path('news/', NewsListView.as_view()),
-    path('news/<int:pk>', NewsDetail.as_view()),
-    path('analytics/<int:pk>', PostDetail.as_view()),
+    path('blog/<int:pk>', PostDetail.as_view()),
 
 
 ]
