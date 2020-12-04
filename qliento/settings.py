3a758ab34cbcb0b853706022359fde3c4f7211d5
Dongolok <dongolokish@gmail.com>
2020-12-05 03:26:13 +0600

    fixing social registration again

--- qliento/settings.py
+++ qliento/settings.py
@@ -273,7 +273,7 @@ SOCIAL_AUTH_FACEBOOK_SECRET = config('SOCIAL_AUTH_FACEBOOK_SECRET')
 
 # Define SOCIAL_AUTH_FACEBOOK_SCOPE to get extra permissions from Facebook.
 # Email is not sent by default, to get it, you must request the email permission.
-SOCIAL_AUTH_FACEBOOK_SCOPE = ['email', 'name']
+SOCIAL_AUTH_FACEBOOK_SCOPE = ['email']
 SOCIAL_AUTH_FACEBOOK_PROFILE_EXTRA_PARAMS = {
     'fields': 'id, name, email'
 }
