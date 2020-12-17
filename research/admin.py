a21680d0af244ef9abc508ed40ae09c3209f3558
Nazgul <ismailova_n@auca.kg>
2020-12-17 19:12:21 +0600

    resize of images and categories

--- research/admin.py
+++ research/admin.py
@@ -163,9 +163,43 @@ class CountryAdmin(TranslationAdmin):
     search_fields = ['name']
 
 
-class CategoryAdmin(TranslationAdmin):
+class CategoryAdmin(DraggableMPTTAdmin, TranslationAdmin):
+    
     form = CategoryForm
-    list_display = ('name', 'parent', )
+
+    mptt_indent_field = "name" 
+    list_display = ('tree_actions', 'indented_title',
+                    'related_researches_count', 'related_researches_cumulative_count')  
+    list_display_links = ('indented_title',)    
+
+    def get_queryset(self, request):    
+        qs = super().get_queryset(request)  
+
+        # Add cumulative product count  
+        qs = Category.objects.add_related_count(    
+                qs, 
+                Research,   
+                'category', 
+                'researches_cumulative_count',  
+                cumulative=True)    
+
+        # Add non cumulative product count  
+        qs = Category.objects.add_related_count(qs, 
+                 Research,  
+                 'category',    
+                 'researches_count',    
+                 cumulative=False)  
+        return qs   
+
+    def related_researches_count(self, instance):   
+        return instance.researches_count    
+    related_researches_count.short_description = 'Исследования в данной категории'  
+
+    def related_researches_cumulative_count(self, instance):    
+        return instance.researches_cumulative_count 
+    related_researches_cumulative_count.short_description = 'Исследования в ветке'  
+    search_fields = ['name']
+
 
 
 class StatusAdmin(TabbedDjangoJqueryTranslationAdmin):
